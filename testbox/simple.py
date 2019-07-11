d775bfa36cd1635681aa1e4650b1490809186848
dopeboi <huzar01x@gmail.com>
2019-07-10 22:46:03 -0400

    new branch for iamges and a derp txt

--- testbox/simple.py
+++
@@ -1,9 +0,0 @@
-user = input("what is your name dawg = ")
-
-def derp():
-    if user == "lil pump":
-        print("eskeitttttt!!!!")
-    else:
-        print("I dont know who you are!")
-
-derp()
