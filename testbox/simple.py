98a03a9529365b0e60b1e261b27120ab12199ec0
dopeboi <huzar01x@gmail.com>
2019-07-10 22:25:08 -0400

    random test to see if i didnt mess up my master repo!

---
+++ testbox/simple.py
@@ -0,0 +1,9 @@
+user = input("what is your name dawg = ")
+
+def derp():
+    if user == "lil pump":
+        print("eskeitttttt!!!!")
+    else:
+        print("I dont know who you are!")
+
+derp()
